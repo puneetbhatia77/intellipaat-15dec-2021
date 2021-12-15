fc4c1df849f72bcfeb456215249f7d44aefcb548
Rejunraj <rejunrajreju@gmail.com>
2021-12-15 13:20:48 +0530

    📝 doc() updating the steps for rpm based distro

--- 2.1_jenkins-install.sh
+++ 2.1_jenkins-install.sh
@@ -16,3 +16,18 @@ sudo ufw allow 8080
 # http://ip_address_or_domain_name:8080
 # /var/lib/jenkins/secrets/initialAdminPassword
 # install suggested plugins
+
+# installation for RPM based distributions
+
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum upgrade
+sudo amazon-linux-extras install epel # for daemonize
+sudo yum install jenkins java-1.8.0-openjdk-devel -y
+sudo service jenkins status
+sudo systemctl start jenkins
+# go to NSG and add inbound port 8080 for Azure cloud
+# go to security tab and then add inbound port 8080 in security group for AWS
+# go to /var/lib/jenkins/secrets/initialAdminPassword and copy the password
